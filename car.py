11ece1e85143fb70e5b972fc6288f57b31670854
sekhartrimurthulu <sekharaddala03@gmail.com>
2025-07-13 16:37:16 +0530

    modified car

--- car.py
+++ car.py
@@ -1,17 +1,23 @@
-user = input("Enter command: ")
-a="help"
-b="start"
-c="stop"
-d="quit"
-if user==a:
-    print("start - car is started")
-    print("stop - car stoped")
-    print("quit - to exit")
-    enter=input("Enter: ")
-    if enter==b:
-        print("car is started")
-    elif enter==c:
-        print("car is stoped")
+command=''
+started= False
+while True:
+    command = input(">").lower()
+    if command =='start':
+        if started:
+            print("car already started")
+        else: 
+            started = True
+            print("car is started..")
+    elif command =='stop':
+        if not started:
+            print("car was already stoped")
+        else:
+            started=False            
+            print("car is stoped..")
+    elif command=='help':
+        print("""\nstart - car was started\nstop - car was stoped\nquit - to exit""")
+    elif command=='quit':
+        print("to exit")
+        break
     else:
-        print("to  exit")
-        
+        print("i don't understand")
